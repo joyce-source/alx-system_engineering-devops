5922c31043969d1e6aeef2847b378ebc56190721
joyce-source <brownjojo@gmail.com>
2023-06-23 02:10:10 -0700

    2_kill_commnd

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,9 @@
+exec { 'killmenow':
+  command     => 'pkill -f killmenow',
+  path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+  onlyif      => 'pgrep -f killmenow',
+  user        => 'www-data',
+  group       => 'www-data',
+  refreshonly => true,
+}
+
