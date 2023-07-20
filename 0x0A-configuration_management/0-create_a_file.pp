77133b27b3ac53223f97f8d77cbee90bf6a83f00
joyce-source <brownjojo96@gmail.com>
2023-07-20 11:57:26 +0000

    user

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# Puppet manifest to create file with specific permission,owner,group,content
-file { '/tmp/school':
-  ensure  => file,
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-}
-
