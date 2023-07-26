91593b424c063e2539800c2e7bb45fb6b1a73083
joyce-source <brownjojo96@gmail.com>
2023-07-26 01:42:54 -0700

    debug_0_task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""Python script that using REST API for given employee IDreturns info about TODO list"""
+"""Python script that using API for employee IDreturns info about TODO list"""
 
 import requests
 import sys
+
 
 def get_employee_todo_progress(employee_id):
     """
     Fetches and displays the TODO list progress for a given employee ID.
 
     Parameters:
-        employee_id (int): The ID of the employee whose TODO list progress is to be fetched.
+        employee_id (int):ID of employee whose TODO list is to be fetched
 
     Returns:
-        None: The function prints the TODO list progress to the standard output.
+        None: The function prints the TODO list progress to standard output
     """
     base_url = 'https://jsonplaceholder.typicode.com'
     user_url = f'{base_url}/users/{employee_id}'
@@ -31,7 +32,8 @@ def get_employee_todo_progress(employee_id):
         done_tasks = [task for task in todos_data if task['completed']]
 
         # Display the progress
-        print(f"Employee {employee_name} is done with tasks({len(done_tasks)}/{total_tasks}):")
+        print(f"Employee {employee_name} is done with tasks({len(done_tasks)}/"
+              f"/{total_tasks}):")
         for task in done_tasks:
             print(f"\t{task['title']}")
     except requests.exceptions.RequestException as e:
@@ -40,6 +42,7 @@ def get_employee_todo_progress(employee_id):
     except (KeyError, IndexError):
         print(f"Employee with ID {employee_id} not found.")
         sys.exit(1)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
