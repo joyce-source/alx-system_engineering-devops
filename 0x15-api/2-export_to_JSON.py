7779829d29dc2d293d51b6ad751df24b95f59c30
joyce-source <brownjojo96@gmail.com>
2023-07-26 02:46:00 -0700

    export_JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""Python script that using API for employee IDreturns info about TODO list"""
+"""Python script to export data in the JSON format"""
 
+import json
 import requests
 import sys
 
 
 def get_employee_todo_progress(employee_id):
     """
-    Fetches and displays the TODO list progress for a given employee ID.
+    Fetches the TODO list progress for a given employee ID.
+    Also exports the TODO list data to a JSON file.
 
     Parameters:
-        employee_id (int):ID of employee whose TODO list is to be fetched
+        employee_id (int):ID of the employee whose TODO list to be fetched.
 
     Returns:
-        None: The function prints the TODO list progress to standard output
+        None: The function prints the TODO list progress to the standard output
     """
     base_url = 'https://jsonplaceholder.typicode.com'
     user_url = f'{base_url}/users/{employee_id}'
@@ -28,14 +30,25 @@ def get_employee_todo_progress(employee_id):
         # Fetch todos for the specified employee
         todos_response = requests.get(todos_url)
         todos_data = todos_response.json()
-        total_tasks = len(todos_data)
-        done_tasks = [task for task in todos_data if task['completed']]
 
         # Display the progress
         print(f"Employee {employee_name} is done with tasks({len(done_tasks)}/"
-              f"/{total_tasks}):")
+              f"{total_tasks}):")
         for task in done_tasks:
             print(f"\t{task['title']}")
+
+        # Export to JSON
+        json_data = {str(employee_id): [
+            {"task": task['title'],
+                "completed": task['completed'],
+                "username": employee_name}
+            for task in todos_data
+        ]}
+        json_filename = f"{employee_id}.json"
+        with open(json_filename, mode='w') as json_file:
+            json.dump(json_data, json_file)
+        print(f"Data exported to {json_filename}")
+
     except requests.exceptions.RequestException as e:
         print("Error:", e)
         sys.exit(1)
@@ -46,7 +59,7 @@ def get_employee_todo_progress(employee_id):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+        print("Usage: python3 2-export_to_JSON.py <employee_id>")
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
