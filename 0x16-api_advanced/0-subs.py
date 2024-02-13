89c4b2e9c44d61770bf30a9b274d63a26281fe2c
joyce-sorce <brownjojo96@gmail.com>
2024-02-13 12:41:17 +0300

    redone

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,29 +12,18 @@ def number_of_subscribers(subreddit):
     Returns:
         int: The number of subscribers for the subreddit, or 0 if the subreddit is invalid or unavailable.
     """
-    # Set a custom User-Agent to avoid Too Many Requests error
-    headers = {'User-Agent': 'Custom User Agent'}
+
+  headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
     # Construct the URL to query the subreddit's information
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+  url = f'https://www.reddit.com/r/{subreddit}/about.json'
 
     # Send a GET request to the Reddit API
-    response = requests.get(url, headers=headers)
+  response = requests.get(url, headers=headers)
 
     # Check if the response is successful
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        except (KeyError, IndexError):
-            # Invalid subreddit or error in API response
-            return 0
-    else:
-        # Invalid subreddit or error in API response
-        return 0
-
-if __name__ == '__main__':
-    subreddit = input("Enter the subreddit: ")
-    num_subscribers = number_of_subscribers(subreddit)
-    print(f"Number of subscribers in '{subreddit}': {num_subscribers}")
+  if response.status_code == 200:
+        data = response.json()
+  return data['data']['subscribers']
+  else:
+      return 0
