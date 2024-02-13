2ddb1175f548bc933b60a7855aa99bc57858aeed
joyce-sorce <brownjojo96@gmail.com>
2024-02-13 12:50:49 +0300

    subs_error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,18 +12,15 @@ def number_of_subscribers(subreddit):
     Returns:
         int: The number of subscribers for the subreddit, or 0 if the subreddit is invalid or unavailable.
     """
-
+  url = f"https://www.reddit.com/r/{subreddit}/about.json"
   headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
-    # Construct the URL to query the subreddit's information
-  url = f'https://www.reddit.com/r/{subreddit}/about.json'
-
-    # Send a GET request to the Reddit API
+      # Send a GET request to the Reddit API
   response = requests.get(url, headers=headers)
 
     # Check if the response is successful
   if response.status_code == 200:
         data = response.json()
-  return data['data']['subscribers']
+        return data['data']['subscribers']
   else:
       return 0
