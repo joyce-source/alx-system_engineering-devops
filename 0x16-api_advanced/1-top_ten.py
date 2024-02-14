80046ce74f57ef5a34c58f645b65a028ea45dc72
joyce-sorce <brownjojo96@gmail.com>
2024-02-14 11:56:04 +0300

    top-ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
+"""script to retrieve titles of the top 10 hot posts for a given subreddit."""
 import requests
 
+
 def top_ten(subreddit):
+    """
+    Retrieve the titles of the top 10 hot posts for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        None: If the subreddit is not found or an error occurs.
+    """
     # Set a custom User-Agent to avoid Too Many Requests error
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'User-Agent': 'Custom User Agent'}
@@ -11,10 +22,10 @@ def top_ten(subreddit):
 
     # Check if the response is successful
     if response.status_code == 200:
-            data = response.json()
-            posts = data['data']['children']
+        data = response.json()
+        posts = data['data']['children']
 
-            for post in posts:
-                print(post['data']['title'])
+        for post in posts:
+            print(post['data']['title'])
     else:
-       print(None)
+        print(None)
