525507061289371d73bbae40b7ded14f98a8e43b
joyce-sorce <brownjojo96@gmail.com>
2024-02-14 12:05:49 +0300

    recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,8 +1,22 @@
-#!/usr/bin/pythoni2
+#!/usr/bin/python3
 import requests
 
+
 def recurse(subreddit, hot_list=[], after=None):
-    # Set a custom User-Agent to avoid Too Many Requests error
+
+    """
+    Recursively retrieve titles of hot posts for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+        hot_list (list, optional): List to store titles of hot posts.
+        after (str, optional): The 'after' parameter for pagination.
+
+    Returns:
+        list or None: A list containing titles of hot posts for the subreddit,
+            or None if the subreddit is not found or an error occurs.
+    """
+# Set a custom User-Agent to avoid Too Many Requests error
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=100'
     if after:
         url += f'&after={after}'
@@ -13,11 +27,11 @@ def recurse(subreddit, hot_list=[], after=None):
 
     # Check if the response is successful
     if response.status_code == 200:
-            data = response.json()
-            posts = data['data']['children']
-            after = data['data']['after']
-            for post in posts:
-                hot_list.append(post['data']['title'])
+        data = response.json()
+        posts = data['data']['children']
+        after = data['data']['after']
+        for post in posts:
+            hot_list.append(post['data']['title'])
 
             if after:
                 return recurse(subreddit, hot_list, after)
